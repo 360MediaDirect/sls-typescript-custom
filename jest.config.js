8414e2859610c80c70b3e7e1d70c7d44d482ddb2
EC2 Default User <ec2-user@ip-172-31-60-200.ec2.internal>
2020-06-09 13:45:36 +0000

    add winston; update to scripts; prettier updates

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,7 @@
 module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    testMatch: ["**/__tests__/**/*.ts?(x)", "**/?(*.)+(test).ts?(x)"]
-  };
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/__tests__/**/*.ts?(x)', '**/?(*.)+(test).ts?(x)'],
+  coverageReporters: ['json', 'lcov', 'text', 'clover'],
+  setupFilesAfterEnv: ['./jest.setup.js']
+}
